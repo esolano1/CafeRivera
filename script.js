d094152ae4910a64ef9ef15d070eb9f598326e3c
esolano1 <joeybello23@gmail.com>
2025-05-06 23:47:42 -0600

    Adding more content

--- script.js
+++ script.js
@@ -24,3 +24,36 @@ buttonRight.addEventListener('click', function(e) {
     });
 });
 
+
+
+const productos = [
+   {texto: "Cafe de grano", link: "Productos-Contacto/productos.html"},
+   {texto: "Cafe en pergamino", link: "Productos-Contacto/productos.html"},
+   {texto: "Cafe molido", link: "Productos-Contacto/productos.html"} 
+];
+  
+
+  const buscador = document.getElementById('searchInput');
+const resultados = document.getElementById('results');
+
+buscador.addEventListener('input', function() {
+    const texto = buscador.value.toLowerCase();
+  
+    if (texto.length >= 3) {
+      // Dividimos el texto en palabras
+      const palabras = texto.split(/\s+/); // divide por espacios
+  
+      const filtrados = productos.filter(item => {
+        const itemLower = item.texto.toLowerCase();
+        // Devuelve true sólo si TODAS las palabras están presentes en el texto
+        return palabras.every(palabra => itemLower.includes(palabra));
+      });
+  
+      resultados.innerHTML = filtrados.length
+    /*    ? filtrados.map(item => `<p>${item}</p>`).join('')  */
+    ? filtrados.map(item => `<a href="${item.link}">${item.texto}</a>`).join('<br>')
+        : '<p>No se encontraron resultados</p>';
+    } else {
+      resultados.innerHTML = '';
+    }
+  });
